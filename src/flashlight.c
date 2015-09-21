31e9ebc498c9da570bfa4198b709049e5a9b529a
IceCube Dev <WesR@users.noreply.github.com>
2015-09-21 15:07:22 -0400

    Fixed the opening message

--- src/flashlight.c
+++ src/flashlight.c
@@ -50,7 +50,7 @@ static void window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press Up to turn on the light");
+  text_layer_set_text(text_layer, "Press Up to turn on");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
